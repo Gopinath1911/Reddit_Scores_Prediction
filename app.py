fdc93a7c9a66b950472ec3ede983efed8b18bc55
Gopinath1911 <72126553+Gopinath1911@users.noreply.github.com>
2020-12-26 23:08:33 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = request.form['comment']
+    prediction = model.predict(int_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Toxic content of the comment {}'.format(output))
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
